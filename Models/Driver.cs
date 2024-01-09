34179a7d65148aa6b3fa6a0131b39f2eee3b92d4
Khumeren R <khucode@gmail.com>
2024-01-10 00:55:56 +0800

    ready to start building, registered dbContext

---
+++ Models/Driver.cs
@@ -0,0 +1,11 @@
+namespace ApiCachingApp.Models;
+
+
+public class Driver{
+
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public int RacingNumber { get; set; }
+    public string Team { get; set; } = string.Empty;
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using ApiCachingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApiDbContext>(options => 
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
