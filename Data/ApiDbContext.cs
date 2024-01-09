c528defc8eaeeda9fe22791aae157776772fc44f
Khumeren R <khucode@gmail.com>
2024-01-10 00:44:54 +0800

    initial commit

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+using ApiCachingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCachingApp.Data;
+
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<Driver> Drivers {get; set;}
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+
+
+    }
+}
